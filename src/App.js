f156fe5b07e91f51185537b883a7be652f663d15
Riley Gramlich <gramlichriley@gmail.com>
2023-01-11 16:07:53 -0700

    add about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Home/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div className="About">
+            <h1 className="title">About Me</h1>
+            <p className="about-me">I am a full stack web developer with a drive to bring useful web applications to people to improve their lives with apps that build efficient user experiences.  I am passionate and motivated to work on problems that improve and progress the lives of people. I would love to work on Artificial General Intelligence in the future and/or contribute to humanity’s exploration of Space.</p>
+            <ul className="skills">Here are some things I excel at:
+                <li className="skill">Javascript</li>
+                <li className="skill">HTML</li>
+                <li className="skill">CSS</li>
+                <li className="skill">React</li>
+                <li className="skill">MondoDB</li>
+                <li className="skill">Express</li>
+                <li className="skill">Node.js</li>
+                <li className="skill">Python</li>
+                <li className="skill">Django</li>
+                <li className="skill">SQL</li>
+                <li className="skill">PostgreSQL</li>
+                <li className="skill">DevOps</li>
+            </ul>
+        </div>
+    )
+}
