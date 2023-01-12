7631b385c6d137f7650c8edfe19dd647ee32e779
Riley Gramlich <gramlichriley@gmail.com>
2023-01-12 12:32:08 -0700

    change header to navabar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <main className="App">
+      <Navbar/>
       <Home/>
       <About/>
       <Projects/>
-    </div>
+    </main>
   );
 }
 
