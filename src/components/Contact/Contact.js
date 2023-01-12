4c7800371d0e8058694b8b8671ae1c15b238a6e4
Riley Gramlich <gramlichriley@gmail.com>
2023-01-12 12:40:34 -0700

    add contact page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Contact() {
+    return (
+        <div className="Contact">
+            <h1 className="title">Contact</h1>
+            <form action="" className="email-form">
+                <label>Email:</label>
+                <input type="text" className="email" /><br />
+                <label htmlFor="">Subject:</label>
+                <input type="text" className="subject" /><br />
+                <label htmlFor="">Content:</label>
+                <input type="text" className="content" /><br />
+                <button type="submit">Send Email</button>
+            </form>
+        </div>
+    )
+}
