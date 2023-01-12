2e84becce81e86ced6c15596e8d7d3cf5e8c0ee4
Riley Gramlich <gramlichriley@gmail.com>
2023-01-12 14:23:00 -0700

    add styling to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,20 @@
-import React from "react"
+import React from "react";
+import "./Home.css";
 
 export default function Home() {
-    return (
-        <div className="Home">
-            <p className="intro">Hi, my name is</p>
-            <h1 className="name">Riley Gramlich</h1>
-            <h3 className="one-liner">I like to problem solve and build good apps</h3>
-            <p className="description">I'm a Full Stack Software Developer interested in advancing my career with cool teams</p>
-        </div>
-    )
+  return (
+    <div className="Home">
+      <div className="intro-content">
+        <p className="intro">Hello, welcome to my porfolio! My name is</p>
+        <h1 className="name">Riley Gramlich</h1>
+        <h3 className="one-liner">
+          I like to problem solve and build good apps
+        </h3>
+        <p className="description">
+          I'm a Full Stack Software Developer interested in advancing my career
+          with cool teams
+        </p>
+      </div>
+    </div>
+  );
 }
